e6df38f5abb71d650d0ef8d6730754c3cd3e04f6
The Natural <philip.smalls86@gmail.com>
2019-05-25 00:07:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+  puts "The line is currently empty."
+  else 
+     message = "The line is currently:"
+     katz_deli.each_with_index do |name, index| 
+     message += " #{index + 1}. #{name}"
+    end 
+    puts "#{message}"
+  end
+end
