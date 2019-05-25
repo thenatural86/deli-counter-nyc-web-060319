e63df6771fa1d438e38351f45033a991762dc6d8
The Natural <philip.smalls86@gmail.com>
2019-05-25 01:09:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,9 +21,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0 
   puts "There is nobody waiting to be served!"
 else 
-  message = "Currently serving"
-  message += " #{katz_deli.shift()}."
-  puts message
+  puts "Currently serving #{katz_deli.shift()}."
   end
 end
 
